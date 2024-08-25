e5adb8b1eea7e20c18b2cf29abf752f94936ef54
kingsleynwafor54 <kingsleynwafor54@gmail.com>
2024-08-25 20:07:38 +0100

    first commit

---
+++ src/main/java/com/kingChudi/security/model/Role.java
@@ -0,0 +1,6 @@
+package com.kingChudi.security.model;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/kingChudi/security/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.kingChudi.security.repository;
+
+import com.kingChudi.security.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+}
